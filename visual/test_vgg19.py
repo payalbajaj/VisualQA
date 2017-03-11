e1ffb35399082e8bb9991a0e8d94406991abd87e
Alisha <alisha.rege@gmail.com>
2017-03-11 11:25:02 -0800

    Visual Feature Extractor

--- visual/test_vgg19.py
+++ visual/test_vgg19.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import vgg19
 import utils
 
-BATCH_SIZE = 50
+BATCH_SIZE = 10
 
 
 filenames = []
@@ -35,15 +35,15 @@ with tf.Session() as sess:
     for step in xrange(size / BATCH_SIZE):
       offset = step * BATCH_SIZE
       batch_data = imageget(offset,offset + BATCH_SIZE)
-      images = tf.placeholder("float", [BATCH_SIZE, 448, 448, 3])
+      images = tf.placeholder("float", [batch_data.shape[0], 448, 448, 3])
       feed_dict = {images: batch_data}
       vgg = vgg19.Vgg19()
       with tf.name_scope("content_vgg"):
       	vgg.build(images)
       output = sess.run(vgg.output, feed_dict=feed_dict)
       for ind, out in enumerate(output):
-	npyout = np.array(out)
-	sumit = np.concatenate(npyout[:])
-	print_visembed(filenames[offset+ind], sumit, f)
+		npyout = np.array(out)
+		sumit = np.concatenate(npyout[:])
+		print_visembed(filenames[offset+ind], sumit, f)
 f.close()
 fi.close()
